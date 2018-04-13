d0d4d57e1187f587b3197471ce55c15d73ad08b1
Outie Dorfling <outie.d@afrihost.com>
2018-04-13 13:10:58 +0200

    fixed game

--- classes/game.php
+++ classes/game.php
@@ -29,6 +29,7 @@ class Game
         2 => 'Thirty',
         3 => 'Forty'
     ];
+
     /**
      * Create a new Tennis instance.
      *
@@ -40,3 +41,54 @@ class Game
         $this->player1 = $player1;
         $this->player2 = $player2;
     }
+
+    /**
+     * @return \player
+     */
+    public function getPlayer1()
+    {
+        return $this->player1;
+    }
+
+    /**
+     * @param \player $player1
+     */
+    public function setPlayer1($player1)
+    {
+        $this->player1 = $player1;
+    }
+
+    /**
+     * @return \player
+     */
+    public function getPlayer2()
+    {
+        return $this->player2;
+    }
+
+    /**
+     * @param \player $player2
+     */
+    public function setPlayer2($player2)
+    {
+        $this->player2 = $player2;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPointsLookup()
+    {
+        return $this->pointsLookup;
+    }
+
+    /**
+     * @param array $pointsLookup
+     */
+    public function setPointsLookup($pointsLookup)
+    {
+        $this->pointsLookup = $pointsLookup;
+    }
+
+    
+}
